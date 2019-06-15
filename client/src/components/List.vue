2f67e1f840c9b0764082b38ac1f6e273c683f1cb
MarkOhnsman <mark.ohnsman@gmail.com>
2019-06-14 21:00:11 -0600

    Couldn't help myself ;)

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -2,25 +2,30 @@
   <div class="col-4">
     <div class="card list-card">
       <div class="card-header list-title">
-        <div class="list-title-text">
-          {{listData.title}}
-        </div>
+        <div class="list-title-text">{{listData.title}}</div>
         <button @click="deleteList" class="btn btn-danger" title="Delete List">
           <i class="fas fa-trash-alt"></i>
         </button>
       </div>
       <!-- Render Here -->
-      <task v-for="task in tasks" :taskData="task" :listData="listData" />
+      <task v-for="task in tasks" :taskData="task" :listData="listData" :key="task._id"/>
 
       <!-- Maybe something will go here -->
 
       <!-- Task From -->
       <form class="add-task-form" @submit.prevent="submitTask">
         <div class="input-group">
-          <input type="text" class="form-control" placeholder="Title" v-model='newTask.title'>
-          <input type="text" class="form-control" placeholder="Description" v-model='newTask.description'>
+          <input type="text" class="form-control" placeholder="Title" v-model="newTask.title">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Description"
+            v-model="newTask.description"
+          >
           <div class="input-button">
-            <button type="submit" class="btn btn-secondary mb-2"><i class="fas fa-plus"></i></button>
+            <button type="submit" class="btn btn-secondary mb-2">
+              <i class="fas fa-plus"></i>
+            </button>
           </div>
         </div>
       </form>
@@ -29,56 +34,54 @@
 </template>
 
 <script>
-  import Task from '@/components/Task.vue'
-  export default {
-    name: 'List',
-    props: ['listData'],
-    data() {
-      return {
-        newTask: {
-          title: '',
-          description: '',
-          listId: this.listData._id
-        }
+import Task from "@/components/Task.vue";
+export default {
+  name: "List",
+  props: ["listData"],
+  data() {
+    return {
+      newTask: {
+        title: "",
+        description: "",
+        listId: this.listData._id
       }
-    },
-    mounted() {
-      this.$store.dispatch('getTasks', this.listData._id)
-    },
-    computed: {
-      tasks() {
-        return this.$store.state.tasks[this.listData._id]
-      }
-    },
-    methods: {
-      deleteList() {
-        this.$store.dispatch('deleteList', this.listData);
-      },
-      submitTask({ target: form }) {
-        this.$store.dispatch('addTask', this.newTask)
-        form.reset()
-      },
-
-    },
-    components: {
-      Task
+    };
+  },
+  mounted() {
+    this.$store.dispatch("getTasks", this.listData._id);
+  },
+  computed: {
+    tasks() {
+      return this.$store.state.tasks[this.listData._id];
     }
-
+  },
+  methods: {
+    deleteList() {
+      this.$store.dispatch("deleteList", this.listData);
+    },
+    submitTask({ target: form }) {
+      this.$store.dispatch("addTask", this.newTask);
+      form.reset();
+    }
+  },
+  components: {
+    Task
   }
+};
 </script>
 
 <style>
-  .list-title {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+.list-title {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .add-task-form {
-    margin: 10px
-  }
+.add-task-form {
+  margin: 10px;
+}
 
-  .list-title-text {
-    font-size: 18px;
-  }
+.list-title-text {
+  font-size: 18px;
+}
 </style>
--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -2,86 +2,93 @@
   <div class="card task">
     <ul class="list-group list-group-flush">
       <li class="list-group-item">
-        <h3>{{taskData.title}}<i class="fas fa-eraser" @click="deleteTask"></i></h3>
+        <h3>
+          {{taskData.title}}
+          <i class="fas fa-eraser" @click="deleteTask"></i>
+        </h3>
         <p>{{taskData.description}}</p>
-
 
         <!-- complete list drop down menu -->
         <select v-model="selected" @change="moveTask">
-          <option disabled value="">Move Task...</option>
+          <option disabled value>Move Task...</option>
           <option v-for="list in lists" :value="list._id">{{list.title}}</option>
         </select>
       </li>
-      <li class="list-group-item">
-
-      </li>
+      <li class="list-group-item"></li>
     </ul>
     <div v-for="(comment, index) in comments" :task="taskData">
-      <p>{{comment.content}} - {{comment.user}}<button class="btn btn-danger"
-          @click="deleteComment(index)">Delete</button>
+      <p>
+        {{comment.content}} - {{comment.user}}
+        <button
+          class="btn btn-danger"
+          @click="deleteComment(index)"
+        >Delete</button>
       </p>
     </div>
     <form class="form-inline" @submit.prevent="submitComment">
       <div class="form-group mx-sm-3 mb-2">
         <input type="text" class="form-control" placeholder="Comments" v-model="newComment.content">
         <input type="text" class="form-control" placeholder="User" v-model="newComment.user">
-        <button class="btn btn-info" type="submit"><i class="fas fa-plus"></i></button>
+        <button class="btn btn-info" type="submit">
+          <i class="fas fa-plus"></i>
+        </button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Task",
-    props: ['taskData', 'listData'],
-    data() {
-      return {
-        newComment: {
-          content: '',
-          user: '',
-          taskId: this.taskData._id,
-          listId: this.taskData.listId
-        },
-        selected: '',
-      }
+export default {
+  name: "Task",
+  props: ["taskData", "listData"],
+  data() {
+    return {
+      newComment: {
+        content: "",
+        user: "",
+        taskId: this.taskData._id,
+        listId: this.taskData.listId
+      },
+      selected: ""
+    };
+  },
+  computed: {
+    comments() {
+      let comments = this.taskData.comments;
+      return comments;
     },
-    computed: {
-      comments() {
-        let comments = this.taskData.comments
-        return comments
-      },
-      lists() {
-        return this.$store.state.lists
-      }
-
-    },
-    methods: {
-      submitComment({ target: form }) {
-        this.taskData.comments.push(this.newComment)
-        this.$store.dispatch('addComment', this.taskData)
-        form.reset()
-      },
-      deleteTask() {
-        this.$store.dispatch('deleteTask', this.taskData);
-      },
-      deleteComment(index) {
-        this.taskData.comments.splice(index, 1)
-        this.$store.dispatch('deleteComment', this.taskData)
-      },
-      moveTask() {
-        this.taskData.oldId = this.taskData.listId
-        this.taskData.listId = this.selected
-        this.$store.dispatch('moveTask', this.taskData)
-      }
+    lists() {
+      return this.$store.state.lists;
     }
-
+  },
+  methods: {
+    submitComment({ target: form }) {
+      this.taskData.comments.push(this.newComment);
+      this.$store.dispatch("addComment", this.taskData);
+      form.reset();
+    },
+    deleteTask() {
+      this.$store.dispatch("deleteTask", this.taskData);
+    },
+    deleteComment(index) {
+      this.taskData.comments.splice(index, 1);
+      this.$store.dispatch("deleteComment", this.taskData);
+    },
+    moveTask() {
+      this.taskData.oldId = this.taskData.listId;
+      this.taskData.listId = this.selected;
+      this.$store.dispatch("moveTask", this.taskData);
+    }
   }
+};
 </script>
 
-<style>
-  .task {
-    color: #3f4430;
-    margin: 20px;
-  }
+<style scoped>
+.task {
+  color: #3f4430;
+  margin: 20px;
+}
+.form-group {
+  max-width: 100%;
+}
 </style>
